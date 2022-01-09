ac1f831e84eba271bcc010f0a4b2645a0bae5545
kherma <kamil.herma01@gmail.com>
2022-01-09 07:30:49 +0100

    Update HomeLink styles

--- components/common/HomeLink.js
+++ components/common/HomeLink.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Link from "next/link";
 import { links } from "../../utils/config";
+import { useRouter } from "next/router";
 
 const HomeLink = () => {
   const { link, Icon } = links.home;
+  const router = useRouter();
   return (
     <nav>
       <Link href={link}>
         <a>
-          <Icon className="p-4 w-20 h-20 paper" />
+          <Icon
+            className={`p-4 w-20 h-20 hover:text-white hover:bg-bgRed-100 transition-colors duration-300 paper ${
+              router.pathname === link && "bg-bgRed-100 text-white"
+            }`}
+          />
         </a>
       </Link>
     </nav>
