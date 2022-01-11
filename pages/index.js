90ca741720d3ddf423eebf50258509d0dfe8add7
kherma <kamil.herma01@gmail.com>
2022-01-11 05:07:45 +0100

    Add WebsiteMotionDesign component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import WebsiteMotionDesign from "../components/feature/WebsiteMotionDesign";
 
 const Home = () => {
   return (
     <Layout pageTitle="home">
-      <h1>Hello Home</h1>
+      <div className="flex overflow-auto flex-col gap-4 p-4 w-full h-full lg:gap-8 lg:p-8 xl:flex-row">
+        <div className="w-full h-full"></div>
+        <WebsiteMotionDesign />
+      </div>
     </Layout>
   );
 };
