44d2c1db57878c5720effd9c38c8b1eb4b9da543
kherma <kamil.herma01@gmail.com>
2022-01-12 17:21:22 +0100

    Remove preload from logo image

--- components/common/Logo.js
+++ components/common/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <h1 className="flex gap-4 justify-center items-center mb-4 text-4xl font-black text-slate-800 uppercase md:mb-8 md:text-5xl lg:justify-start">
       <div className="relative w-16 h-16 md:w-20 md:h-20 lg:w-28 lg:h-28">
-        <Image src="/logo.png" alt="logo" layout="fill" priority />
+        <Image src="/logo.png" alt="logo" layout="fill" />
       </div>
       patchwork
     </h1>
