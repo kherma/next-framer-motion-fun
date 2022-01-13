3e00eb51f67e0465c26cd3a04c3d5d8cc6a11be8
kherma <kamil.herma01@gmail.com>
2022-01-14 00:35:13 +0100

    Add WebMotionDesing buttons animations

--- components/feature/WebsiteMotionDesign/FooterWMD.js
+++ components/feature/WebsiteMotionDesign/FooterWMD.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { btnScaleAnim } from "../../../animations/btnScaleAnim";
+import { motion } from "framer-motion";
 
 const FooterWMD = ({ handleOpen }) => {
+  const { whileHover, whileTap, transition } = btnScaleAnim(false, 0.9, 1.05);
+
   return (
-    <div
+    <motion.div
+      whileHover={whileHover}
+      whileTap={whileTap}
+      transition={transition}
       onClick={handleOpen}
       className="w-full h-1/5 bg-blue-400 rounded-2xl cursor-pointer"
     />
--- components/feature/WebsiteMotionDesign/SearchBarWMD.js
+++ components/feature/WebsiteMotionDesign/SearchBarWMD.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { AiOutlineSearch } from "react-icons/ai";
+import { btnScaleAnim } from "../../../animations/btnScaleAnim";
 
 const SearchBarWMD = () => {
   const [toggleSearch, setToggleSearch] = useState(false);
+  const { whileHover, whileTap, transition } = btnScaleAnim(false, 0.9, 1.1);
   return (
     <motion.div
       onClick={() => setToggleSearch(!toggleSearch)}
+      whileHover={whileHover}
+      whileTap={whileTap}
+      transition={transition}
       className="relative h-8 bg-slate-400 rounded-full cursor-pointer"
       animate={{
         width: toggleSearch ? "250px" : "34px",
