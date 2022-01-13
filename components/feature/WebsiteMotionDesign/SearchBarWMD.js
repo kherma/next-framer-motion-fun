a82fc43bca7b9f4cff689345d53750e1bc489612
kherma <kamil.herma01@gmail.com>
2022-01-13 21:50:31 +0100

    Add animation to SearchBarWMD

--- components/feature/WebsiteMotionDesign/SearchBarWMD.js
+++ components/feature/WebsiteMotionDesign/SearchBarWMD.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBarWMD = () => {
+  const [toggleSearch, setToggleSearch] = useState(false);
   return (
-    <div className="w-full h-4 bg-slate-400 rounded-2xl cursor-pointer md:h-8" />
+    <motion.div
+      onClick={() => setToggleSearch(!toggleSearch)}
+      className="relative h-8 bg-slate-400 rounded-full cursor-pointer"
+      animate={{
+        width: toggleSearch ? "300px" : "34px",
+        transition: {
+          transition: {
+            duration: 0.5,
+            type: "spring",
+            sctiffness: 300,
+          },
+        },
+      }}
+    >
+      <AiOutlineSearch className="absolute top-1/2 left-2 w-4 h-4 text-white -translate-y-1/2" />
+    </motion.div>
   );
 };
 
