3533e90d8fe5963f9dced1c33e0d0581aeaa0c69
kherma <kamil.herma01@gmail.com>
2022-01-15 21:36:36 +0100

    Add generateName function

--- seed/data/generateName.js
+++ seed/data/generateName.js
@@ -1,3 +1,24 @@
+import { randomRange } from "../utils/randomRange";
+
 export const generateName = (config) => {
-  console.log(config);
+  const { letters } = config.settings;
+
+  for (let i = 0; i < 12; i++) {
+    if (i <= 6) {
+      i === 6
+        ? (config.data.name += "-")
+        : (config.data.name +=
+            letters[randomRange(0, letters.length - 1)].toUpperCase());
+    }
+
+    if (i > 6 && i <= 10) {
+      i === 10
+        ? (config.data.name += "-")
+        : (config.data.name += randomRange(0, 9));
+    }
+
+    if (i > 10) {
+      config.data.name += letters[randomRange(0, letters.length - 1)];
+    }
+  }
 };
