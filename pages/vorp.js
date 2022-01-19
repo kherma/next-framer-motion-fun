268f09f9f20ebd324d6dc50df871e862cd4b1db6
kherma <kamil.herma01@gmail.com>
2022-01-19 11:37:59 +0100

    Add VorpPet

--- pages/vorp.js
+++ pages/vorp.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import VorpPet from "../components/feature/VorpPet";
 
 const Vorp = () => {
   return (
     <Layout pageTitle="vorp">
-      <h1>Hello Vorp</h1>
+      <div className="w-full h-full bg-red-500">
+        <VorpPet />
+      </div>
     </Layout>
   );
 };
