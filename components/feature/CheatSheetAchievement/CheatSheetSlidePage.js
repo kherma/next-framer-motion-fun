82a4d1a097469b5c4f759326adf8cb552b032a28
kherma <kamil.herma01@gmail.com>
2022-01-29 21:03:52 +0100

    Update CheatSheet page for xl devices

--- components/feature/CheatSheetAchievement/CheatSheetSlidePage.js
+++ components/feature/CheatSheetAchievement/CheatSheetSlidePage.js
@@ -1,13 +1,13 @@
 const CheatSheetSlidePage = ({ title, commands }) => {
   return (
-    <div className="py-4 px-2 w-full h-full rounded-2xl sm:p-6 xl:p-8">
-      <h3 className="mb-4 text-lg font-black text-black dark:text-white uppercase bg-white dark:bg-black border-b border-b-black dark:border-b-white transition-colors duration-300 ease-in-out sm:mb-6 sm:text-2xl xl:text-3xl">
+    <div className="py-4 px-2 w-full h-full rounded-2xl sm:p-6 xl:p-4 2xl:p-8">
+      <h3 className="mb-4 text-lg font-black text-black dark:text-white uppercase bg-white dark:bg-black border-b border-b-black dark:border-b-white transition-colors duration-300 ease-in-out sm:mb-6 sm:text-2xl xl:text-2xl 2xl:text-3xl">
         {title}
       </h3>
       <ul className="flex flex-col gap-1 ml-2">
         {commands.map((command, index) => (
           <li
-            className="text-xs text-black dark:text-white lowercase bg-white dark:bg-black transition-colors duration-300 ease-in-out sm:text-base xl:text-lg"
+            className="text-xs text-black dark:text-white lowercase bg-white dark:bg-black transition-colors duration-300 ease-in-out sm:text-base xl:text-base 2xl:text-lg"
             key={index}
           >
             {command}
